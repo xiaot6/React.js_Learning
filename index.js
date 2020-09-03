f6ff2ff1a695fcb4d3f089fd367c671b3a1569af
Xiao Tan <xiaot6@illinois.edu>
2020-09-02 21:56:59 -0500

    .

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log('hello world');//logging something
+
+let name = 'pangda'; //declare a variable
+console.log(name);
+
+//How to name a variable:
+//cannot be a reserved keyword
+//Should be meaningful
+//Cannot start with a number
+//Cannot contains saoce or Hyphen
+//Are case sensitive;
+
+// let interestRate = 0.2; //可以变的变量
+//
+// interestRate = 1;
+//
+// const interestRate = 0.3; // 不能变的变量
